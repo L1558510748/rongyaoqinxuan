84703eb6eb944f1ff01d2421ab3bf1ca5f056713
lin <1558510748@qq.com>
2020-03-31 11:49:05 +0800

    放大镜1

--- src/js/productdetail.js
+++ src/js/productdetail.js
@@ -35,7 +35,27 @@ class Detail {
         $(".srollpic").css("display", "block");
     }
     move(e){
-        var l=e.pageX-$("")
+        console.log($('.hoverpic').width())
+        var l=e.pageX-$(".bigpic").offset().left-$('.hoverpic').width()/2;
+        var t=e.pageY-$(".bigpic").offset().top-$(".hoverpic").height()/2;
+        if(l<0)l=0;
+        if(t<0)t=0;
+        if(l>$('.bigpic').width()-$(".hoverpic").width()){
+            l=$('.bigpic').width()-$(".hoverpic").width();
+        }
+        if(t>$('.bigpic').height()-$(".hoverpic").height()){
+            t=$('.bigpic').height()-$(".hoverpic").height();
+        }
+        $(".hoverpic").css("left",l+"px");
+        $(".hoverpic").css("top",t+"px");
+        var x=l/($(".bigpic").width()-$(".hoverpic").width());
+        var y=t/($(".bigpic").height()-$(".hoverpic").height());
+        $(".srollpic").find("img").css("left",$(".srollpic").width()-$(".srollpic").find("img").width()*x+"px")
+        $(".srollpic").css("top",$(".srollpic").height()-$(".srollpic").find("img").height()*y+"px")
+    }
+    out(){
+        $(".hoverpic").css("display","none");
+        $(".srollpic").css("display","none");
     }
     getdata(){
         var that=this;
@@ -67,6 +87,7 @@ class Detail {
         $("#sp1").attr("src",this.data.img1)
         $("#sp2").attr("src",this.data.img2)
         $("#sp3").attr("src",this.data.img3)
+        $(".srollpic").find("img").attr("src",this.data.img1);
     }
     getUrlParam(name) {
         var reg = new RegExp("(^|&)" + name + "=([^&]*)(&|$)"); //构造一个含有目标参数的正则表达式对象
